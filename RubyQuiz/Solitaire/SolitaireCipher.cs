5f48c50eff15f7e1dacec9ef5670dac2f26bb227
Matt Burke <burkemd1+github@gmail.com>
2014-11-11 21:39:49 -0600

    working decryption

--- RubyQuiz/Solitaire/SolitaireCipher.cs
+++ RubyQuiz/Solitaire/SolitaireCipher.cs
@@ -28,9 +28,21 @@ namespace RubyQuiz.Solitaire
             return new CharacterGroup(key.GetNext(), key.GetNext(), key.GetNext(), key.GetNext(), key.GetNext());
         }
 
-        public string Decrypt(Deck key, string encryptedMessage)
+        public string Decrypt(Deck key, string message)
         {
-            throw new NotImplementedException();
+            var stream = new DeckKeystream(key.Clone());
+            var messageGroups = CharacterGroup.CreateSequence(MessageSanitizer.Sanitize(message));
+
+            StringBuilder buffer = new StringBuilder();
+            foreach (var group in messageGroups)
+            {
+                var messageNumberGroup = group.ToNumberGroup();
+                var keystreamNumberGroup = nextStreamGroup(stream).ToNumberGroup();
+                var resultGroup = messageNumberGroup.Subtract(keystreamNumberGroup);
+                buffer.Append(resultGroup.ToCharacterGroup());
+            }
+
+            return buffer.ToString();
         }
     }
 }
