e59eb944b82b3bd539229e92b25c2a2eb27a35e6
Matt Burke <burkemd1+github@gmail.com>
2014-11-14 23:11:49 -0600

    interface for the various algorithms to be later implemented

---
+++ src/RubyQuiz/Santa/ISantaAssigner.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RubyQuiz.Santa
+{
+    public interface ISantaAssigner
+    {
+        IEnumerable<Assignment> Assign(IEnumerable<Person> people);
+    }
+}
